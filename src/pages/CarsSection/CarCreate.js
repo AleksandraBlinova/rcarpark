5006cc50967184cc8451395aca73cc2dedbb3115
aleksa-blinova mail.ru <aleksa-blinova@mail.ru>
2021-04-28 15:28:40 +0300

    "carcreate" was changed

--- src/pages/CarsSection/CarCreate.js
+++ src/pages/CarsSection/CarCreate.js
@@ -1,8 +1,47 @@
 import React from 'react';
 import axios from 'axios';
-
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import InputBase from '@material-ui/core/InputBase';
 import './CarCreate.css'
-const CarCreate = ({ addCar }) => {
+
+
+const BootstrapInput = withStyles((theme) => ({
+    root: {
+      'label + &': {
+        marginTop: theme.spacing(6),
+      },
+    },
+    input: {
+      borderRadius: 4,
+      backgroundColor: theme.palette.background.paper,
+      border: '1px solid #ced4da',
+      fontSize: 16,
+      transition: theme.transitions.create(['border-color', 'box-shadow']),
+      // Use the system font instead of the default Roboto font.
+      fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(','),
+      '&:focus': {
+        borderRadius: 4,
+        borderColor: '#80bdff',
+        boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+      },
+    },
+  }))(InputBase);
+
+function CarCreate ({ addCar })  {
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -13,33 +52,69 @@ const CarCreate = ({ addCar }) => {
             })
             .catch(console.error);
     };
-
+   
     return (
-        <React.Fragment>
-            <h1 className='h1'>Добавление нового авто</h1>
-            <form className='form-container' onSubmit={handleSubmit}>
-            <div >
-            <div className='input-css'>
-            <label className='label'>Модель: </label>
-            <input  type="text" name="model" placeholder="Выберите модель:" />
-            </div>  
-            <div className='input-css'>
-            <label className='label'>Цвет: </label>
-            <input  type="text" name="color" placeholder="Выберите цвет:" />
-            </div>  
-            <div className='input-css'>
-            <label className='label'>Цена: </label>
-            <input  type="text" name="price" placeholder="Введите цену:" />
-            </div>  
-            <div className='input-css'>
-            <label className='label'>Год выпуска: </label>
-            <input type="text" name="releaseYear" placeholder="Введите год выпуска:" />
-            </div>
-            <button className='create-btn' type="submit">Создать</button>
-            </div>
-              
-            </form>
-        </React.Fragment >
+      
+    <React.Fragment>
+     <h1 className='h1'>Добавление нового авто</h1>
+    <div >
+    <form className='form-container1' onSubmit={handleSubmit}>
+    <div>
+          
+          <div className='combobox'>
+            <FormControl >
+            <InputLabel htmlFor="demo-customized-select-native">Модель</InputLabel>
+            <NativeSelect >
+              <option aria-label="None" value="" />
+              <option value={10}>Ten</option>
+              <option value={20}>Twenty</option>
+              <option value={30}>Thirty</option>
+            </NativeSelect>
+          </FormControl>
+          </div>
+          <div className='combobox'>
+          <FormControl >
+          <InputLabel htmlFor="demo-customized-select-native">Цвет</InputLabel>
+          <NativeSelect>
+            <option aria-label="None" value="" />
+            <option value={10}>Ten</option>
+            <option value={20}>Twenty</option>
+            <option value={30}>Thirty</option>
+          </NativeSelect>
+        </FormControl>
+        </div>
+
+        <div className='label'>
+        <FormControl >
+        <InputLabel htmlFor="demo-customized-textbox">Цена</InputLabel>
+        <BootstrapInput id="demo-customized-textbox" />
+        </FormControl>
+        </div>
+
+        <div className='label'>
+        <FormControl >
+        <InputLabel htmlFor="demo-customized-textbox">Год выпуска</InputLabel>
+        <BootstrapInput id="demo-customized-textbox" />
+        </FormControl>
+        </div>
+
+        <div className='label'>
+        <FormControl >
+        <InputLabel htmlFor="demo-customized-textbox">Фото</InputLabel>
+        <BootstrapInput id="demo-customized-textbox" />
+        </FormControl>
+        </div>
+
+        <div>
+        <button className='btn btn-primary' type="submit">Создать</button>
+        </div>
+
+    </div>
+    </form>
+    </div>
+
+           
+    </React.Fragment >
     )
 }
 
